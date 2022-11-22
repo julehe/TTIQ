2f859134f40a0a2e3e0f3f2ae0f91423250b7658
julehe <96024164+julehe@users.noreply.github.com>
2022-11-22 13:52:46 +0100

    Update max_realpart.py

--- max_realpart.py
+++ max_realpart.py
@@ -1,17 +1,19 @@
-# This function approximates solutions to the characteristic equation 
-# of the linearization of the DDE model for different parameter settings.
-# It returns the realpart of the solution with maximal realpart.
-#
-# The second input params should contain parameters of the model in the 
-# following order:
-# tracing coverage, rel. freq. of test. in U2, rel. freq. of test. in Q,
-# strict. of isol., strict. of quarantine, tracing delay, scal. fac.
-# for presym. infectiousness, dur. of lat. phase, dur. of presym. phase, 
-# total dur. of inf. phase, basic repr. nbr.
-#
-# PRCC is a boolean determining whether the code is run 
-# for the PRCC analysis in which case complQ is determined by the value of 
-# complI.
+"""
+This function approximates solutions to the characteristic equation 
+of the linearization of the DDE model for different parameter settings.
+It returns the realpart of the solution with maximal realpart.
+
+The second input params should contain parameters of the model in the 
+following order:
+tracing coverage, rel. freq. of test. in U2, rel. freq. of test. in Q,
+strict. of isol., strict. of quarantine, tracing delay, scal. fac.
+for presym. infectiousness, dur. of lat. phase, dur. of presym. phase, 
+total dur. of inf. phase, basic repr. nbr.
+
+PRCC is a boolean determining whether the code is run 
+for the PRCC analysis in which case complQ is determined by the value of 
+complI.
+"""
 
 import numpy as np
 from cheb import cheb
